2171ffdad93992728cb527b4757044b09f658d0d
Bachtiar Dwi Pramudi <bchtrrprmd@gmail.com>
2025-07-05 12:18:23 +0700

    📙 | First usage on Filament v4.x.

--- app/Providers/Filament/SisfoCtlPanelProvider.php
+++ app/Providers/Filament/SisfoCtlPanelProvider.php
@@ -27,9 +27,12 @@ class SisfoCtlPanelProvider extends PanelProvider
             ->default()
             ->id('sisfo-ctl')
             ->path('sisfo-ctl')
+            ->favicon(asset('polije-logo-single.png'))
+            ->brandName('SISFOctl - POLIJE')
+            ->passwordReset()
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Blue,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
